442ef05cdfacb75570c11abc2863f1ce79c69d21
Stalin Chiguano <schiguano@metropolitan-touring.com>
2022-05-14 23:59:39 -0500

    fix: varios temas en una plantilla

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,15 @@ Y
 package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
 H
 server/models/Periodo.js,7\d\7da0467ad15f0635d0b952cbd3bbd5ae640581a4
+P
+ server/routes/reportes.routes.js,8\6\86bec916320839b14d0f0641305131c1ecf86320
+M
+server/models/Calificacion.js,1\a\1a2726075383a38bca8c58cca59650a4414cbf54
+O
+server/routes/carrera.routes.js,d\3\d305112cb29e0ac9cf91cb034f45fbca3e937bea
+T
+$server/routes/calificacion.routes.js,f\2\f29b218cb6e807211ed2738cbfb28b55d4bd7c5d
+K
+server/models/Comentario.js,6\9\6961f9270443a9ee44701cb85026db3169d48ad4
+W
+'server/controllers/reporteController.js,7\f\7fd6f7817bdb27623790ae5326534986e2031ae1
--- server/models/Plantilla.js
+++ server/models/Plantilla.js
@@ -1,69 +1,68 @@
 const mongoose = require('mongoose')
 
-const { Schema, model } = mongoose
+const {Schema, model} = mongoose
 
 const PlantillaSchema = new Schema(
-  {
-    codigo: {
-      type: String,
-      trim: true,
-      lowercase: true,
-    },
-    titulo: {
-      type: String,
-      required: true,
-      trim: true,
-      lowercase: true,
-    },
-    numero: {
-      type: Number,
-      required: true,
-    },
+    {
+        codigo: {
+            type: String,
+            trim: true,
+            lowercase: true,
+        },
+        titulo: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+        },
+        numero: {
+            type: Number,
+            required: true,
+        },
 
-    formato: {
-      type: String,
-      trim: true,
-      required: true,
-      lowercase: true,
+        formato: {
+            type: String,
+            trim: true,
+            required: true,
+            lowercase: true,
+        },
+        requisitos: {
+            type: [String],
+            trim: true,
+            required: true,
+        },
+        instrucciones: {
+            type: [String],
+            trim: true,
+            required: true,
+        },
+        resultados: {
+            type: [String],
+            trim: true,
+            required: true,
+        },
+        objetivos: {
+            type: [String],
+            trim: true,
+            required: true,
+        },
+        temas: {
+            type: [{type: Schema.Types.ObjectId, ref: 'Tema'}],
+        },
+        coordinador: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'Usuario',
+        },
+        asignatura: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'Asignatura',
+        },
     },
-    requisitos: {
-      type: [String],
-      trim: true,
-      required: true,
-    },
-    instrucciones: {
-      type: [String],
-      trim: true,
-      required: true,
-    },
-    resultados: {
-      type: [String],
-      trim: true,
-      required: true,
-    },
-    objetivos: {
-      type: [String],
-      trim: true,
-      required: true,
-    },
-    temas: {
-      type: Schema.Types.ObjectId,
-      ref: 'Tema',
-    },
-    coordinador: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: 'Usuario',
-    },
-    asignatura: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: 'Asignatura',
-    },
-  },
-  {
-    versionKey: false,
-  }
+    {
+        versionKey: false,
+    }
 )
 
 module.exports = model('Plantilla', PlantillaSchema)
